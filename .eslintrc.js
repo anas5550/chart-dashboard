d2996d181625b184a58338bead65b51eb5ca7a44
anas siddiqui <anas91612@gmail.com>
2025-06-10 20:17:03 +0530

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'react-app', // CRA already includes react-hooks plugin
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  rules: {
+    // your custom ESLint rules here
+  },
+};
