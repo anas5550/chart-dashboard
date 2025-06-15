04ca2232935d7bf1af7781864656f7e3e74e584e
anas siddiqui <anas91612@gmail.com>
2025-06-15 17:27:26 +0530

    all changes are done

--- src/components/Charts/PerformanceLineChart.js
+++ src/components/Charts/PerformanceLineChart.js
@@ -25,27 +25,6 @@ const PerformanceLineChart = ({ selectedMetricsForChart }) => {
   const [error, setError] = useState(null);
   const [apiResponseMessage, setApiResponseMessage] = useState('');
 
-  const colors = [
-    '#8884d8',
-    '#82ca9d',
-    '#ffc658',
-    '#ff7300',
-    '#00b0ff',
-    '#f77f00',
-    '#39e6a3',
-    '#e60026',
-    '#1a75ff',
-    '#ff0073',
-    '#9933ff',
-    '#66cc99',
-    '#c06c84',
-    '#6c5b7b',
-    '#355c7d',
-    '#f67280',
-    '#f8b195',
-    '#355c7d',
-  ];
-
   useEffect(() => {
     // Log the selected metrics whenever the effect runs
     console.log(
