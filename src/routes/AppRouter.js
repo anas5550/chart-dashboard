3d942364d84a16ebc515d6e6e7942ebf1e250cde
anas siddiqui <anas91612@gmail.com>
2025-06-16 10:18:37 +0530

    changed appRouter to fix deployment issue

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoutes from './ProtectedRoutes';
 import PublicRoutes from './PublicRoutes';
 import routesConfig from './routesConfig';
